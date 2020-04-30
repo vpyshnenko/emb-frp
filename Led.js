d4a3d6bd6a2f096c2f92eb1f560cf9aaa2bf4808
vpyshnenko <vpyshnenko@gmail.com>
2020-04-30 14:28:38 +0300

    Initial commit

---
+++ Led.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default ({ color, state }) => (
+  <div>
+  <h1>Hello {name}!</h1>;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-d2ojlx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-d2ojlx)
