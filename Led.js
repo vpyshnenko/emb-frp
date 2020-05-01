0a56c6335f546cb5214caa15e9ed48ebefa6351e
vpyshnenko <vpyshnenko@gmail.com>
2020-05-01 20:13:29 +0300

    led

--- Led.js
+++ Led.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames'
 
-export default ({ color, state }) => (
-  <div>
-  <h1>Hello {name}!</h1>;
+export default ({ color, state$ }) => {
+  const [state, setState] = useState(false);
+  useEffect(
+  () => {
+    const subscription = state$.subscribe(setState);
+    return () => {
+      subscription.unsubscribe();
+    };
+  },
+  [state$],
+);
+  return (<div className={classNames('led', state ? "led-on" : "led-off")}>
+  </div>
+)
+}
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
+import Led from './Led';
 import './style.css';
+import { of } from 'rxjs';
+import { interval } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+const led1$ = interval(1000).pipe(map(v => {
+  console.log('V ', v)
+  return v%2
+}),take(4))
 
 class App extends Component {
   constructor() {
@@ -14,6 +23,7 @@ class App extends Component {
   render() {
     return (
       <div>
+      <Led state$={led1$}/>
         <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
